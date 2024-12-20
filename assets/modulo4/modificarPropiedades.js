14234e4d853253ffc4a0824b20bc04d247932232
Katheryn Guasca <Katheryng2017@gmail.com>
2024-12-20 17:14:40 -0500

    Agregando nuevos archivos

---
+++ assets/modulo4/modificarPropiedades.js
@@ -0,0 +1,8 @@
+function cambiarColor(){
+    document.getElementById("p1").style.color = "blue";
+  };
+  
+  function cambiarTexto(){
+    var texto = document.getElementById("p1");
+    texto.style.fontWeight = "bold";
+  };
