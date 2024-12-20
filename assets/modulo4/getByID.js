ede103ac2696137462710f3eafc0e45a0db87904
Katheryn Guasca <Katheryng2017@gmail.com>
2024-12-20 16:56:37 -0500

    Agregando archivos js, html y css de getByID

---
+++ assets/modulo4/getByID.js
@@ -0,0 +1,9 @@
+function cambiaTexto(){
+    var elem = document.getElementById("rect");
+    elem.innerHTML = "Nuevo texto";
+  };
+  
+  function cambiaTexto2(){
+    var elem = document.getElementById("rect");
+    elem.innerHTML = "<a href=\"https://www.google.com\">Google</a>";
+  };
